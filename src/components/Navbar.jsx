f7b818b39066878f7a6627f4b54274a4da080d6d
SindreSau <sindre.sauarlia@outlook.com>
2023-01-27 10:54:31 +0100

    commit before redoing navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink as Link } from 'react-router-dom'
 import logo from '../../src/assets/images/nav-logo.png'
 
 function Navbar({ fixed }) {
-  const [navbarOpen, setNavbarOpen] = React.useState(false);
+  //Functionality for navbar responsiveness
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      const ismobile = window.innerWidth < 768;
+      if (ismobile !== isMobile) {
+        setIsMobile(ismobile)
+      }
+    }, false)
+  }, [isMobile])
+
+  const [toggle, setToggle] = useState(false)
+  const classOnMenuClick = () => {
+    setToggle(!toggle)
+    const navList = document.querySelector('#navList');
+    if (toggle) {
+      navList.classList = 'nav-dropdown';
+    } else {
+      navList.classList = ('nav-dropdown nav-dropdown-show');
+    }
+  }
+
+  // Add color to the selected list-item
   let activeClassName = "clr-primary";
+
+  // return
   return (
     <>
-      <nav className='relative px-10 py-8 bg-white border'>
+      <nav className='relative bg-white'>
 
-        <div className='w-full relative flex justify-between'>
+        <div className='w-full px-10 py-10 flex justify-between'>
           {/* LOGO */}
           <Link to='/'><img src={logo} alt='logo-image'></img></Link>
           {/* Mobile dropdown */}
-          <button className='text-gray-400 md:hidden' onClick={() => setNavbarOpen(!navbar)}>
+          <button className='text-gray-400 md:hidden' onClick={classOnMenuClick}>
             <span className='sr-only'></span>
             <span className="material-symbols-outlined ">
               menu
             </span>
           </button>
           {/* Desktop menu */}
-          <ul className={'flex items-center gap-24 max-md:hidden'}>
+          <ul id='navList' className={isMobile ? 'nav-dropdown' : 'flex items-center gap-24'}>
             <li>
               <Link to='/' className={({ isActive }) => isActive ? activeClassName : undefined}>
                 Home
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <h1>Home</h1>
+    <h1 className='bg-primary h-52'>Home</h1>
   )
 }
 
