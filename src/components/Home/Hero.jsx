d6c7864e6a06c398f2044a9259f3335d9aa4f05a
SindreSau <sindre.sauarlia@outlook.com>
2023-01-27 22:27:24 +0100

    tailwind line-height not working, check in build

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -8,14 +8,14 @@ const Hero = () => {
     <>
       {/* Hero container */}
       <img src={heroBackground} className='home-hero-image'></img>
-      <div className='relative flex flex-col items-center py-8 md:py-20'>
+      <div className='relative flex flex-col items-center py-8 md:py-24'>
 
         {/* hero text and buttons */}
-        <div className='content-max-width flex flex-col justify-center px-1 items-center gap-5 md:gap-8'>
+        <div className='content-max-width flex flex-col justify-center px-1 items-center gap-5 md:gap-11'>
           <h3 className='ff-montserrat-700  md:text-2xl clr-grey md:text-white'>
             Food app
           </h3>
-          <h1 className='md:ff-poppins-700 ff-montserrat-700 md:text-6xl text-4xl text-center md:text-white clr-secondary'>
+          <h1 className='md:ff-poppins-700 ff-montserrat-700 leading-5 md:text-6xl text-4xl text-center md:text-white clr-secondary'>
             Why stay hungry when you can order from Bella Onojie
           </h1>
           <h4 className='ff-montserrat-400 text-2xl text-center text-gray-500 md:text-white'>
@@ -32,7 +32,7 @@ const Hero = () => {
         </div>
 
         {/* hero phone-images */}
-        <div className='w-full h-36 py-11'>
+        <div className='w-full h-36 py-12'>
           {/* imagecontainer */}
           <div className='relative app-preview-container'>
             <div className='app-preview app-preview-left'>
